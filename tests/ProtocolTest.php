3e82789b9e3dac75fa5047d4923003f2b2e83cf1
jeyroik <jeyroik@gmail.com>
2020-05-05 23:40:07 +0300

    tests

--- tests/ProtocolTest.php
+++ tests/ProtocolTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace tests;
 
-
+use extas\components\extensions\Extension;
+use extas\components\extensions\ExtensionRepository;
+use extas\components\extensions\ExtensionRepositoryGet;
 use extas\components\plugins\Plugin;
 use extas\components\plugins\PluginRepository;
 use extas\components\protocols\Protocol;
@@ -9,6 +11,7 @@ use extas\components\protocols\ProtocolParameterHeaderDefault;
 use extas\components\protocols\ProtocolRepository;
 use extas\components\protocols\ProtocolRunner;
 use extas\components\SystemContainer;
+use extas\interfaces\extensions\IExtensionRepositoryGet;
 use extas\interfaces\repositories\IRepository;
 use extas\interfaces\stages\IStageProtocolRunAfter;
 use PHPUnit\Framework\TestCase;
@@ -23,6 +26,7 @@ class ProtocolTest extends TestCase
 {
     protected IRepository $pluginRepo;
     protected IRepository $protocolRepo;
+    protected IRepository $extRepo;
 
     protected function setUp(): void
     {
@@ -30,6 +34,7 @@ class ProtocolTest extends TestCase
         $env = Dotenv::create(getcwd() . '/tests/');
         $env->load();
         $this->protocolRepo = new ProtocolRepository();
+        $this->extRepo = new ExtensionRepository();
         $this->pluginRepo = new class extends PluginRepository {
             public function reload()
             {
@@ -46,6 +51,7 @@ class ProtocolTest extends TestCase
             ProtocolEmpty::class
         ]]);
         $this->protocolRepo->delete([Protocol::FIELD__NAME => 'test']);
+        $this->extRepo->delete([Extension::FIELD__CLASS => ExtensionRepositoryGet::class]);
     }
 
     public function testBasicMethods()
@@ -62,6 +68,12 @@ class ProtocolTest extends TestCase
 
     public function testRunner()
     {
+        $this->extRepo->create(new Extension([
+            Extension::FIELD__CLASS => ExtensionRepositoryGet::class,
+            Extension::FIELD__INTERFACE => IExtensionRepositoryGet::class,
+            Extension::FIELD__SUBJECT => '*',
+            Extension::FIELD__METHODS => ['protocolRepository']
+        ]));
         $this->pluginRepo->create(new Plugin([
             Plugin::FIELD__CLASS => ProtocolEmpty::class,
             Plugin::FIELD__STAGE => IStageProtocolRunAfter::NAME
