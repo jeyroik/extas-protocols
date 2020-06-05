93eba5d9d554a2ca37d0578f1164a5ed5e88cdda
jeyroik <jeyroik@gmail.com>
2020-06-05 16:56:13 +0300

    up to installer v3

--- tests/ProtocolTest.php
+++ tests/ProtocolTest.php
@@ -1,57 +1,47 @@
 <?php
 namespace tests;
 
-use extas\components\extensions\Extension;
+use extas\interfaces\stages\IStageProtocolRunAfter;
+
 use extas\components\extensions\ExtensionRepository;
-use extas\components\extensions\ExtensionRepositoryGet;
+use extas\components\http\TSnuffHttp;
 use extas\components\plugins\Plugin;
 use extas\components\plugins\PluginRepository;
 use extas\components\protocols\Protocol;
 use extas\components\protocols\ProtocolParameterHeaderDefault;
 use extas\components\protocols\ProtocolRepository;
 use extas\components\protocols\ProtocolRunner;
-use extas\components\SystemContainer;
-use extas\interfaces\extensions\IExtensionRepositoryGet;
-use extas\interfaces\repositories\IRepository;
-use extas\interfaces\stages\IStageProtocolRunAfter;
+use extas\components\repositories\TSnuffRepository;
+
 use PHPUnit\Framework\TestCase;
 use Dotenv\Dotenv;
-use Psr\Http\Message\RequestInterface;
-use Slim\Http\Headers;
-use Slim\Http\Request;
-use Slim\Http\Stream;
-use Slim\Http\Uri;
 
+/**
+ * Class ProtocolTest
+ *
+ * @package tests
+ * @author jeyroik <jeyroik@gmail.com>
+ */
 class ProtocolTest extends TestCase
 {
-    protected IRepository $pluginRepo;
-    protected IRepository $protocolRepo;
-    protected IRepository $extRepo;
+    use TSnuffRepository;
+    use TSnuffHttp;
 
     protected function setUp(): void
     {
         parent::setUp();
         $env = Dotenv::create(getcwd() . '/tests/');
         $env->load();
-        $this->protocolRepo = new ProtocolRepository();
-        $this->extRepo = new ExtensionRepository();
-        $this->pluginRepo = new class extends PluginRepository {
-            public function reload()
-            {
-                parent::$stagesWithPlugins = [];
-            }
-        };
-        SystemContainer::addItem('protocolRepository', ProtocolRepository::class);
+        $this->registerSnuffRepos([
+            'protocolRepository' => ProtocolRepository::class,
+            'pluginRepository' => PluginRepository::class,
+            'extensionRepository' => ExtensionRepository::class
+        ]);
     }
 
     protected function tearDown(): void
     {
-        $this->pluginRepo->reload();
-        $this->pluginRepo->delete([Plugin::FIELD__CLASS => [
-            ProtocolEmpty::class
-        ]]);
-        $this->protocolRepo->delete([Protocol::FIELD__NAME => 'test']);
-        $this->extRepo->delete([Extension::FIELD__CLASS => ExtensionRepositoryGet::class]);
+        $this->unregisterSnuffRepos();
     }
 
     public function testBasicMethods()
@@ -61,31 +51,25 @@ class ProtocolTest extends TestCase
         $this->assertEquals(['*'], $protocol->getAccept());
         $protocol->setClass(ProtocolEmpty::class);
         $data = [];
-        $protocol($data, $this->getRequest());
+        $protocol($data, $this->getPsrRequest());
         $this->assertArrayHasKey('test', $data);
         $this->assertEquals('is ok', $data['test']);
     }
 
     public function testRunner()
     {
-        $this->extRepo->create(new Extension([
-            Extension::FIELD__CLASS => ExtensionRepositoryGet::class,
-            Extension::FIELD__INTERFACE => IExtensionRepositoryGet::class,
-            Extension::FIELD__SUBJECT => '*',
-            Extension::FIELD__METHODS => ['protocolRepository']
-        ]));
-        $this->pluginRepo->create(new Plugin([
+        $this->createWithSnuffRepo('pluginRepository', new Plugin([
             Plugin::FIELD__CLASS => ProtocolEmpty::class,
             Plugin::FIELD__STAGE => IStageProtocolRunAfter::NAME
         ]));
-        $this->protocolRepo->create(new Protocol([
+        $this->createWithSnuffRepo('protocolRepository', new Protocol([
             Protocol::FIELD__NAME => 'test',
             Protocol::FIELD__CLASS => ProtocolEmpty::class,
             Protocol::FIELD__ACCEPT => ['*']
         ]));
 
         $data = [];
-        ProtocolRunner::run($data, $this->getRequest());
+        ProtocolRunner::run($data, $this->getPsrRequest());
 
         $this->assertArrayHasKey('test', $data);
         $this->assertEquals('is ok again', $data['test']);
@@ -100,7 +84,7 @@ class ProtocolTest extends TestCase
             ProtocolParameterHeaderDefault::FIELD__PROTOCOL_KEY => 'test'
         ]);
         $data = [];
-        $protocol($data, $this->getRequest());
+        $protocol($data, $this->getPsrRequest());
         $this->assertArrayHasKey('test', $data);
         $this->assertEquals('is ok', $data['test']);
 
@@ -111,7 +95,7 @@ class ProtocolTest extends TestCase
             ProtocolParameterHeaderDefault::FIELD__PROTOCOL_KEY => 'test2'
         ]);
         $data = [];
-        $protocol($data, $this->getRequest());
+        $protocol($data, $this->getPsrRequest());
         $this->assertArrayHasKey('test2', $data);
         $this->assertEquals('ok', $data['test2']);
 
@@ -122,26 +106,8 @@ class ProtocolTest extends TestCase
             ProtocolParameterHeaderDefault::FIELD__PROTOCOL_KEY => 'test3'
         ]);
         $data = [];
-        $protocol($data, $this->getRequest());
+        $protocol($data, $this->getPsrRequest());
         $this->assertArrayHasKey('test3', $data);
         $this->assertEmpty($data['test3']);
-    }
-
-    /**
-     * @return RequestInterface
-     */
-    protected function getRequest(): RequestInterface
-    {
-        return new Request(
-            'GET',
-            new Uri('http', 'localhost', 80, '/', 'test2=ok'),
-            new Headers([
-                'Content-type' => 'text/html',
-                ProtocolParameterHeaderDefault::HEADER__PREFIX . 'test' => 'is ok'
-            ]),
-            [],
-            [],
-            new Stream(fopen('php://input', 'r'))
-        );
     }
 }
